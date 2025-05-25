e5e492a6e4bf2a4ac208ca8ffbb28a30fa74924f
liayp <amaliaypanane@gmail.com>
2025-05-25 22:00:55 +0800

    fixing button add to cart

--- src/components/menu/AddToCartButton.js
+++ src/components/menu/AddToCartButton.js
@@ -1,29 +1,31 @@
-import FlyingButton from 'react-flying-item'
+import FlyingButton from 'react-flying-item';
 
 export default function AddToCartButton({
-  hasSizesOrExtras, onClick, basePrice, image
+    hasSizesOrExtras, onClick, basePrice, image
 }) {
-  if (!hasSizesOrExtras) {
+    if (!hasSizesOrExtras) {
+        return (
+            <div className="flying-button-parent mt-4">
+                <FlyingButton
+                    className="mt-4 bg-primary hover:bg-yellow-600 text-white border-none rounded-full px-8 py-2"
+                    targetTop={'5%'}
+                    targetLeft={'95%'}
+                    src={image}
+                    onClick={onClick}
+                >
+                    Add to cart Rp. {basePrice}
+                </FlyingButton>
+            </div>
+        );
+    }
+
     return (
-      <div className="flying-button-parent mt-4">
-        <button className="mt-4 bg-primary hover:bg-yellow-600 text-white border-none rounded-full px-8 py-2"
-          targetTop={'5%'}
-          targetLeft={'95%'}
-          src={image}>
-          <div onClick={onClick}>
-            Add to cart Rp. {basePrice}
-          </div>
+        <button
+            type="button"
+            onClick={onClick}
+            className="mt-4 bg-primary hover:bg-yellow-600 text-white border-none rounded-full px-8 py-2"
+        >
+            <span>Add to cart Rp. {basePrice}</span>
         </button>
-      </div>
     );
-  }
-  return (
-    <button
-      type="button"
-      onClick={onClick}
-      className="mt-4 bg-primary hover:bg-yellow-600 text-white border-none rounded-full px-8 py-2"
-    >
-      <span>Add to cart Rp. {basePrice} </span>
-    </button>
-  );
 }
