9a0a6e68fc59c926d4e16e20ba03b0e7a73236c7
aqshalibr <aqshalibrahim27gmail.com>
2025-05-26 00:46:53 +0800

    fix button add to cart

--- src/components/menu/AddToCartButton.js
+++ src/components/menu/AddToCartButton.js
@@ -1,34 +1,25 @@
 'use client';
 
 import React from 'react';
-import FlyingButton from './ClientFlyingButton';
 
 export default function AddToCartButton({
-    hasSizesOrExtras, onClick, basePrice, image
+  hasSizesOrExtras,
+  onClick,
+  basePrice,
 }) {
-    if (!hasSizesOrExtras) {
-        return (
-            <div className="flying-button-parent mt-4">
-                <FlyingButton
-                    className="mt-4 bg-primary hover:bg-yellow-600 text-white border-none rounded-full px-8 py-2"
-                    targetTop={'5%'}
-                    targetLeft={'95%'}
-                    src={image}
-                    onClick={onClick}
-                >
-                    Add to cart Rp. {basePrice}
-                </FlyingButton>
-            </div>
-        );
-    }
+  const formattedPrice = new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+  }).format(basePrice);
 
-    return (
-        <button
-            type="button"
-            onClick={onClick}
-            className="mt-4 bg-primary hover:bg-yellow-600 text-white border-none rounded-full px-8 py-2"
-        >
-            <span>Add to cart Rp. {basePrice}</span>
-        </button>
-    );
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="mt-4 bg-primary hover:bg-yellow-600 text-white border-none rounded-full px-8 py-2"
+    >
+      Add to cart {formattedPrice}
+    </button>
+  );
 }
--- src/components/menu/ClientFlyingButton.js
+++
@@ -1,5 +0,0 @@
-'use client';
-
-import FlyingButton from 'react-flying-item';
-
-export default FlyingButton;
